139d29b89c53e68d1b5442068ac423a866795b71
AndrijShwed <a_shwed@ukr.net>
2024-02-25 13:05:01 +0200

    2541

--- DataBase/Головна.cs
+++ DataBase/Головна.cs
@@ -22,42 +22,42 @@ namespace DataBase
         private void ButtonНаселення_MouseClick(object sender, MouseEventArgs e)
         {
             Населення form = new Населення();
-            this.Hide();
+            //this.Hide();
             form.Show();
         }
 
         private void ButtonНаселеніПункти_MouseClick(object sender, MouseEventArgs e)
         {
             Населені_Пункти form = new Населені_Пункти();
-            this.Hide();
+            //this.Hide();
             form.Show();
         }
 
         private void ПерейменуванняВулиць_MouseClick(object sender, MouseEventArgs e)
         {
             ПерейменуванняВулиць form = new ПерейменуванняВулиць();
-            this.Hide();
+            //this.Hide();
             form.Show();
         }
 
         private void ButtonДомогосподарства_MouseClick(object sender, MouseEventArgs e)
         {
             Домогосподарства form = new Домогосподарства();
-            this.Hide();
+            //this.Hide();
             form.Show();
         }
 
         private void rjButtonДодати_Click(object sender, EventArgs e)
         {
             ХудобаТаПтиця form = new ХудобаТаПтиця();
-            this.Hide();
+            //this.Hide();
             form.Show();
         }
 
         private void rjButton1_MouseClick(object sender, MouseEventArgs e)
         {
             ПочатокРоботи form = new ПочатокРоботи();
-            this.Hide();
+            //this.Hide();
             form.Show();
         }
     }
